964bb607e0f765f955fc2cf414f63ed02ea68311
ErrorCode0 <me@errorcodezero.dev>
2026-05-23 18:47:52 -0700

    adding terminal ansi codes as C macroes

---
+++ src/console.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_SRC_CONSOLE_H_
+#define INCLUDE_SRC_CONSOLE_H_
+
+#define CONSOLE_RED "\033[31m"
+#define CONSOLE_GREEN "\033[32m"
+#define CONSOLE_YELLOW "\033[33m"
+#define CONSOLE_BLUE "\033[34m"
+#define CONSOLE_MAGENTA "\033[35m"
+#define CONSOLE_CYAN "\033[36m"
+#define CONSOLE_WHITE "\033[37m"
+#define CONSOLE_RESET "\033[0m"
+
+#endif
